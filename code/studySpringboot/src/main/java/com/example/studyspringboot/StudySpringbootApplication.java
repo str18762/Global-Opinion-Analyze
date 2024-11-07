7581421ce30921a10ecb152fe9c030ae6c1b6440
ruimx <1553929261@qq.com>
2024-11-07 17:33:08 +0800

    11.7/17:32

--- code/studySpringboot/src/main/java/com/example/studyspringboot/StudySpringbootApplication.java
+++ code/studySpringboot/src/main/java/com/example/studyspringboot/StudySpringbootApplication.java
@@ -6,6 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudySpringbootApplication {
 
+    /**
+     * 程序的入口点
+     * 使用Spring Boot框架启动应用程序
+     *
+     * @param args 命令行参数，允许向程序传递外部参数
+     */
     public static void main(String[] args) {
         SpringApplication.run(StudySpringbootApplication.class, args);
     }
